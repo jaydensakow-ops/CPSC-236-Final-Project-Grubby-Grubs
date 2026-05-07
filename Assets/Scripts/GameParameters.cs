7355be677c6b54fb5159cd2450ff6f03c9afd713
Jayden Sakow <jaydensakow@gmail.com>
2026-05-07 12:54:09 -0700

    After Day 4

--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -3,4 +3,6 @@ using UnityEngine;
 public static class GameParameters
 {
     public static float RodMoveSpeed = 5f;
+    public static float minimumYRodDistance = -2.7f;
+    public static float maximumYRodDistance = 2.7f;
 }
